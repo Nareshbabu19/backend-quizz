fdbe293e8b5b25ca4feb1fec86b2147497b6abf9
nareshbabu <nareshbabuofficial@gmail.com>
2020-12-16 22:39:21 +0530

    changeport

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const userRouter = require("./routes/user");
 require("./db/mongoose");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT | 3000;
 
 app.use(express.json());
 app.use(
