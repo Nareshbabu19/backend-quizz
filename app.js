5d93cd96ee914005ef8ef6927fdbdb7c77bfab26
nareshbabu <nareshbabuofficial@gmail.com>
2020-12-17 13:43:27 +0530

    connect

--- app.js
+++ app.js
@@ -8,22 +8,21 @@ require("./db/mongoose");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use(function(req,res,next){
-//     res.header
-// });
+app.use(cors());
 
 var corsOptions = {
   origin: "https://quizz-app-511d6.web.app/",
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  optionsSuccessStatus: 200,
 };
 
 app.get("/products/:id", cors(corsOptions), function (req, res, next) {
   res.json({ msg: "This is CORS-enabled for only example.com." });
 });
 
-app.listen(80, function () {
-  console.log("CORS-enabled web server listening on port 80");
+app.listen(20, function () {
+  console.log("CORS-enabled web server listening on port 20");
 });
+
 app.use(express.json());
 app.use(
   express.urlencoded({
@@ -40,8 +39,4 @@ app.listen(port, () => {
 
 app.get("/products/:id", cors(), function (req, res, next) {
   res.json({ msg: "This is CORS-enabled for a Single Route" });
-});
-
-app.listen(80, function () {
-  console.log("CORS-enabled web server listening on port 80");
 });
